8b64cabcc26b9d8381442837dbe0f17792eac9c9
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-01-07 12:18:14 -0300

    tabela operador simplificada

--- src/main/java/naturagro/models/Produto.java
+++ src/main/java/naturagro/models/Produto.java
@@ -1,7 +1,9 @@
 package naturagro.models;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -9,6 +11,8 @@ import java.time.LocalDate;
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Produto {
 
     @Id
@@ -16,10 +20,17 @@ public class Produto {
     private String nome;
     private String descricao;
     private String categoria;
-    private LocalDate dataVencimento;
     private LocalDate dataEntrada;
+    private LocalDate dataVencimento;
     private Integer quantidadeEmEstoque;
     private Double precoVarejo;
     private Double precoAtacado;
 
+    private void calculaVencimento() {
+        //todo
+        //we can use .plusDays(numberOfDays);
+        String[] dataEntradaStr = dataEntrada.toString().split("-");
+        Integer dataVencimento = Integer.parseInt(dataEntradaStr[2]) + 2;
+        LocalDate dataVence = LocalDate.of(2025, 01, dataVencimento);
+    }
 }
