98c40007871904dc229d3a670c5775fdb7aeab43
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-01-27 12:38:02 -0300

    entities venda e operador adicionadas

--- src/main/java/naturagro/models/Venda.java
+++ src/main/java/naturagro/models/Venda.java
@@ -1,26 +1,45 @@
 package naturagro.models;
 
-import lombok.Data;
+import lombok.*;
 
 import javax.persistence.*;
+import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
-
 
 @Entity
 @Data
+@NoArgsConstructor
+@Transactional
 public class Venda {
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private LocalDateTime dataCompra;
+    private Double valorTotal;
 
     @ManyToOne
     @JoinColumn(name="operador_id")
     private Operador operador;
 
-    @OneToMany
-    private List<Produto> produtos;
-    private Double valorTotal;
+    @ManyToMany(cascade = CascadeType.ALL)
+    private List<Produto> produtos = new ArrayList<>();
 
+    public Venda(LocalDateTime dataCompra, Double valorTotal, Operador operador, List<Produto> produtos) {
+        this.dataCompra = dataCompra;
+        this.valorTotal = valorTotal;
+        this.operador = operador;
+        this.produtos = produtos;
+    }
+
+    /*
+    @JoinTable(name = "vendas_itens",
+    joinColumns = @JoinColumn(name = "venda_id"),
+    inverseJoinColumns = @JoinColumn(name = "produto_id"))
+
+    produtoService.obterPorID(produto.getId());
+        oS.obterPorID(produto.getId());
+     */
 }
