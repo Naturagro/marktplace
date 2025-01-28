149a9085e7fd28b494ce279239fdf4a7fdae3a33
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-01-27 23:25:38 -0300

    id autoincremented

--- src/main/java/naturagro/models/Operador.java
+++ src/main/java/naturagro/models/Operador.java
@@ -1,20 +1,21 @@
 package naturagro.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import lombok.*;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 
-@Data
+
+@Getter
+@Setter
 @Entity
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class Operador {
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String cpf;
