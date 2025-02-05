cc35532a73a81c2dff7e0c2006110fe6a936828d
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-02-05 00:33:45 -0300

    enum para categorias dos produtos

---
+++ src/main/java/com/naturagro/models/CategoriaProduto.java
@@ -0,0 +1,14 @@
+package com.naturagro.models;
+
+public enum CategoriaProduto {
+    Frutas,
+    Verduras,
+    Carnes,
+    Peixes,
+    Laticinios,
+    Bebidas,
+    Alimentos,
+    Higiene,
+    Limpeza,
+    Cosmeticos
+}
