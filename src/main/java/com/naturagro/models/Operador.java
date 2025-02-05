911e2b04a6f077ef23ebd0cdaa34768b7dfffae3
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-02-04 23:01:22 -0300

    getters, settes, construtor padrão e equals com base em id

--- src/main/java/com/naturagro/models/Operador.java
+++ src/main/java/com/naturagro/models/Operador.java
@@ -1,72 +1,56 @@
 package com.naturagro.models;
 
-import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 
-
-@Getter
-@Setter
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
 public class Operador {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-    @Getter
     private String cpf;
 
-    //construtor com validação de cpf
+    public Operador() {}
+
     public Operador(String nome, String cpf) {
         this.nome = nome;
-        if (validarCPF(cpf)) {
-            this.cpf = cpf;
-        } else {
-            throw new IllegalArgumentException("CPF inválido!");
-        }
+        this.cpf = cpf;
     }
 
-    public boolean validarCPF(String cpf) {
-        // Remove caracteres não numéricos
-        cpf = cpf.replaceAll("[^0-9]", "");
-
-        // Verifica se tem 11 dígitos
-        if (cpf.length() != 11) return false;
-
-        // Evita q tenha os dígitos iguais
-        if (cpf.matches("(\\d)\\1{10}")) return false;
-
-        // Cálculo do primeiro dígito verificador
-        int soma = 0;
-        for (int i = 0; i < 9; i++) {
-            soma += (cpf.charAt(i) - '0') * (10 - i);
-        }
-        int primeiroDigito = 11 - (soma % 11);
-        if (primeiroDigito >= 10) primeiroDigito = 0;
-
-        // Cálculo do segundo dígito verificador
-        soma = 0;
-        for (int i = 0; i < 10; i++) {
-            soma += (cpf.charAt(i) - '0') * (11 - i);
-        }
-        int segundoDigito = 11 - (soma % 11);
-        if (segundoDigito >= 10) segundoDigito = 0;
-
-        // Compara os dígitos calculados com os fornecidos
-        return (primeiroDigito == (cpf.charAt(9) - '0') && segundoDigito == (cpf.charAt(10) - '0'));
+    public String getNome() {
+        return nome;
     }
-    //metodo set (o get foi declarado com lombok na linha 21)
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
     public void setCpf(String cpf) {
-        if (validarCPF(cpf)) {
-            this.cpf = cpf;
-        } else {
-            throw new IllegalArgumentException("CPF inválido!");
-        }
+        this.cpf = cpf;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Operador operador = (Operador) o;
+        return Objects.equals(id, operador.id);
+    }
 }
