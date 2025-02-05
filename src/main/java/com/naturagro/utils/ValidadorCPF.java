d570acdff177c67d678fc7673513f03817561fae
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-02-04 23:00:47 -0300

    validador de CPF

---
+++ src/main/java/com/naturagro/utils/ValidadorCPF.java
@@ -0,0 +1,34 @@
+package com.naturagro.utils;
+
+public class ValidadorCPF {
+
+    public boolean validarCPF(String cpf) {
+        // Remove caracteres não numéricos
+        cpf = cpf.replaceAll("[^0-9]", "");
+
+        // Verifica se tem 11 dígitos
+        if (cpf.length() != 11) return false;
+
+        // Evita q tenha os dígitos iguais
+        if (cpf.matches("(\\d)\\1{10}")) return false;
+
+        // Cálculo do primeiro dígito verificador
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
+            soma += (cpf.charAt(i) - '0') * (10 - i);
+        }
+        int primeiroDigito = 11 - (soma % 11);
+        if (primeiroDigito >= 10) primeiroDigito = 0;
+
+        // Cálculo do segundo dígito verificador
+        soma = 0;
+        for (int i = 0; i < 10; i++) {
+            soma += (cpf.charAt(i) - '0') * (11 - i);
+        }
+        int segundoDigito = 11 - (soma % 11);
+        if (segundoDigito >= 10) segundoDigito = 0;
+
+        // Compara os dígitos calculados com os fornecidos
+        return (primeiroDigito == (cpf.charAt(9) - '0') && segundoDigito == (cpf.charAt(10) - '0'));
+    }
+}
