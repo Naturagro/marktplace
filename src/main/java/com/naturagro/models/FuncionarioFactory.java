5b8cc46df733559daa48cb6912e48405ce459432
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-02-06 11:52:56 -0300

    Factory para criação de funcionarios

---
+++ src/main/java/com/naturagro/models/FuncionarioFactory.java
@@ -0,0 +1,18 @@
+package com.naturagro.models;
+
+
+public class FuncionarioFactory {
+    private static Funcionario funcionario = new Funcionario();
+
+    public Funcionario criarGerente(String nome, String cpf, String senha) {
+        return funcionario.gerente(nome, cpf, senha);
+    }
+
+    public Funcionario criarOperador(String nome, String cpf, String senha) {
+        return funcionario.gerente(nome, cpf, senha);
+    }
+
+    public Funcionario criarEstoquista(String nome, String cpf, String senha) {
+        return funcionario.gerente(nome, cpf, senha);
+    }
+}
