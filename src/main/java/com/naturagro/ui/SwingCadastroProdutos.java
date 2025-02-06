d2e44674d37a55ad5eb1f48575f5a8c696059bca
Pedro-hashm <pedrosadultos@hotmail.com>
2025-02-06 15:49:41 -0300

    Swing Reutilizavel para botões de adicionar

--- src/main/java/com/naturagro/ui/SwingCadastroProdutos.java
+++ src/main/java/com/naturagro/ui/SwingCadastroProdutos.java
@@ -6,18 +6,20 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SwingCadastroProdutos extends JFrame {
-    private static final long serialVersionUID = 1L;
-    private JTextField textField;
-    private JLabel label;
-    private JTable table;
+	private static final long serialVersionUID = 1L;
+	private JTextField textField;
+	private JLabel label;
+	private JTable table;
 	private ControladorSwing controlador;
 
 
-	// Tela
-    public SwingCadastroProdutos(ControladorSwing controlador) {
-    	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	// Criando a Tela
+	public SwingCadastroProdutos(ControladorSwing controlador) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Menu Inicial");
 		setBounds(0, 0, 1280, 720);
 		JPanel contentPane = new JPanel();
@@ -26,62 +28,83 @@ public class SwingCadastroProdutos extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLayeredPane camadas = new JLayeredPane();
 		contentPane.add(camadas);
 		camadas.setBounds(0,0,1280,720);
-		
+
 		ImageIcon logo = new ImageIcon(getClass().getResource("/images/logo.png"));
 		JLabel logoLabel = new JLabel(logo);
 		logoLabel.setBounds(15,23,98,100);
 		camadas.add(logoLabel,Integer.valueOf(1));
-		
+
 		JLabel CadastroProdutosLabel = new JLabel("Cadastro de Produtos");
 		CadastroProdutosLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 36));
 		CadastroProdutosLabel.setForeground(new Color(255, 255, 255));
 		CadastroProdutosLabel.setBounds(123, 46, 362, 44);
 		camadas.add(CadastroProdutosLabel,Integer.valueOf(1));
-		
-		JButton SalvarButton = new JButton("Salvar");
-		SalvarButton.setBackground(new Color(133,179,58));
-		SalvarButton.setForeground(new Color(255,255,255));
-		SalvarButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 30));
-		SalvarButton.setBounds(119, 595, 240, 50);
-		camadas.add(SalvarButton, Integer.valueOf(3));
-		
+
+		JButton AdicionarButton = new JButton("Adicionar");
+		AdicionarButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				// Aqui você chama o JDialog (SwingAdicionar)
+				Map<JTextField, String> campos = new HashMap<>();
+				JTextField nomeProduto = new JTextField();
+				JTextField preco = new JTextField();
+				JTextField codigo = new JTextField();
+				JTextField validade = new JTextField();
+
+				// Preenche o mapa com os campos e labels
+				campos.put(nomeProduto, "Nome do Produto:");
+				campos.put(preco, "Preço:");
+				campos.put(codigo, "Código:");
+				campos.put(validade, "Validade do produto:");
+
+				// Agora, cria o JDialog e o exibe
+				SwingAdicionar dialog = new SwingAdicionar(campos);
+				dialog.setVisible(true); // Exibe o JDialog
+			}
+		});
+		AdicionarButton.setBackground(new Color(133,179,58));
+		AdicionarButton.setForeground(new Color(255,255,255));
+		AdicionarButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 30));
+		AdicionarButton.setBounds(119, 595, 240, 50);
+		camadas.add(AdicionarButton, Integer.valueOf(3));
+
 		JButton EditarButton = new JButton("Editar");
 		EditarButton.setForeground(Color.WHITE);
 		EditarButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 30));
 		EditarButton.setBackground(new Color(133, 179, 58));
 		EditarButton.setBounds(378, 595, 240, 50);
 		camadas.add(EditarButton);
-		
+
 		JButton ExcluirButton = new JButton("Excluir");
 		ExcluirButton.setForeground(Color.WHITE);
 		ExcluirButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 30));
 		ExcluirButton.setBackground(new Color(133, 179, 58));
 		ExcluirButton.setBounds(637, 595, 240, 50);
 		camadas.add(ExcluirButton);
-		
+
 		JButton CancelarButton = new JButton("Cancelar");
 		CancelarButton.setForeground(Color.WHITE);
 		CancelarButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 30));
 		CancelarButton.setBackground(new Color(133, 179, 58));
 		CancelarButton.setBounds(896, 595, 240, 50);
 		camadas.add(CancelarButton);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(122, 125, 1014, 433);
 		camadas.add(scrollPane);
-		
+
 		ImageIcon background2 = new ImageIcon(getClass().getResource("/images/background2edit.png"));
 		JLabel backgroundLabel = new JLabel(background2);
 		backgroundLabel.setBounds(0, 0, 1270, 681);
 		camadas.add(backgroundLabel,Integer.valueOf(0));
-		
+
 		table = new JTable();
 		scrollPane.setViewportView(table);
 
-		
-    }
+
+	}
 }
