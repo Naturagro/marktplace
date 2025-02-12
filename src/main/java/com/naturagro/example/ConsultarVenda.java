7089479ac2db49cdaded772fe49e1c29580668d9
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-02-12 00:52:07 -0300

    detalhamento do consultarvenda

--- src/main/java/com/naturagro/example/ConsultarVenda.java
+++ src/main/java/com/naturagro/example/ConsultarVenda.java
@@ -7,12 +7,18 @@ import com.naturagro.service.VendaService;
 public class ConsultarVenda {
     public static void main(String[] args) {
         VendaService vendaService = new VendaService();
-        Venda v = vendaService.obterPorID(36l);
+        //A parte mais simples na gerência da entidade Venda
+
+        Venda v = vendaService.obterPorID(41l);
+        //Para consultar basta passar o id como parâmetros
+        //Teremos como retorno o objeto com todos os seus atributos para serem consultados
 
         for (Produto produto : v.getProdutos()) {
-            System.out.println(produto.getNome());
+            System.out.println(produto.getNome() + " " + produto.getPrecoVarejo());
         }
         System.out.println();
         System.out.println(v.getOperador().getNome());
+
+        System.out.println(v.obterValorTotal());
     }
 }
