2040ee333ca862b242b64a5458638894e01a13cc
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-02-14 07:38:06 -0300

    aviso

--- src/main/java/com/naturagro/service/FuncionarioService.java
+++ src/main/java/com/naturagro/service/FuncionarioService.java
@@ -3,14 +3,24 @@ package com.naturagro.service;
 import com.naturagro.DAO.DAO;
 import com.naturagro.models.Funcionario;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
 import java.util.List;
-
-//todo: alterar a tabela automaticamente pra o id ser autoincrement
-@Table(name = "funcionarios") //tabela genérica de teste
 public class FuncionarioService extends DAO<Funcionario> {
 
+    /*
+    A classe funcionário herda todos os metodos de DAO logo é contraproducente
+    recriar métodos que já existem na outra classe
+
+
+    Essa classe não pode ser uma tabela no banco de dados, não foi feita para isso
+    Nossa entidade está localizada no pacote models
+    Lá constam todas as configurações necessárias, além de que o ID, por padrão da nossa aplicação
+    já foi setado como auto incremetal, quaisquer tester que preciser ser realizados com a criação de funcionários
+    podem ser feitos através da classe FuncionarioFactory e persistidos no banco e na tabela respectiva
+    com FuncionarioService através do metodo incluir() ou incluirAtomico()
+
+    As únicas adicções que precisa ser feitas nesta classe são adições de métodos de pesquisa
+    favor mantê-la inalterada
+     */
     public FuncionarioService() {
         super(Funcionario.class);
     }
@@ -33,23 +43,4 @@ public class FuncionarioService extends DAO<Funcionario> {
     public List<Funcionario> consultarVendas(Long id) {
         return consultar("Funcionario.vendas", id);
     }
-
-
-    /*
-    Para atualizar o funcionario
-    chamar o metodo mesclar()
-
-    Para deletar funcionario só chamar
-    remover()
-    */
-
-
-
-    public void adicionarFuncionarioBanco(Funcionario funcionario){
-        abrirT();
-        incluir(funcionario);
-        fecharT();
-        fechar();
-    }
-
 }
