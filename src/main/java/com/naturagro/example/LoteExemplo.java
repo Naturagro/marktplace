aac877d52d084eb61cb41291b37f43abedb41de3
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-02-16 15:42:45 -0300

    lote exemplo e explicação

---
+++ src/main/java/com/naturagro/example/LoteExemplo.java
@@ -0,0 +1,30 @@
+package com.naturagro.example;
+
+import com.naturagro.models.Lote;
+import com.naturagro.models.Produto;
+import com.naturagro.service.LoteService;
+import com.naturagro.service.ProdutoService;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class LoteExemplo {
+    public static void main(String[] args) {
+
+        /*
+        Lote e Produto são duas entidades correlatas
+        cada loté conterá uma referência a um tipo de produto
+        Podem existir vários lotes de produtos
+
+        A entidade lote conhece a produto, por chave extrangeira
+        Mas é desconhecida por Produto
+         */
+        ProdutoService produtoService = new ProdutoService();
+        List<Produto> produtos = produtoService.obterTodos();
+
+        Lote lote = new Lote(produtos.get(1), LocalDate.now(), 50);
+
+        LoteService loteService = new LoteService();
+        loteService.incluirAtomico(lote);
+    }
+}
