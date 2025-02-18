c1495f67c5f3184e8ca5befe6c6bd0fb8a77600d
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-02-18 11:10:57 -0300

    método para consultar lote por pedido e quantidade, deverá ser usado na venda

--- src/main/java/com/naturagro/service/LoteService.java
+++ src/main/java/com/naturagro/service/LoteService.java
@@ -2,9 +2,33 @@ package com.naturagro.service;
 
 import com.naturagro.DAO.DAO;
 import com.naturagro.models.Lote;
+import com.naturagro.models.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 public class LoteService extends DAO<Lote> {
     public LoteService() {
         super(Lote.class);
     }
+
+    public Lote consultarLotePorProduto(Produto produto, int quantidade) {
+//        Lote lote = consultarUm("Lote.consultarPorProdutoQntd", quantidade, produto.getId());
+//        return lote;
+        String consulta = "SELECT l FROM Lote l WHERE l.produto.id = :produtoId AND l.quantidade >= :quantidade";
+        EntityManager em = getEntityManager();
+        TypedQuery<Lote> query = em.createQuery(consulta, Lote.class);
+        query.setParameter("quantidade", quantidade);
+        query.setParameter("produtoId", produto.getId());
+
+        return query.getSingleResult();
+
+    }
+//
+//        <named-query name="Lote.consultarPorProdutoQntd">
+//            <query>SELECT l FROM Lote l WHERE l.quantidade > :quantidade and l.produto_id = :produto_id ORDER BY
+//    l.quantidade ASC</query>
+//    </named-query>
+
+
 }
