3e502cb8d930faae417661711eb2d0965c500e89
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-02-20 12:12:39 -0300

    inserção de lote debugada

--- src/main/java/com/naturagro/example/AdicionarLote.java
+++ src/main/java/com/naturagro/example/AdicionarLote.java
@@ -16,14 +16,26 @@ public class AdicionarLote {
             List<Produto> produtos = produtoService.obterTodos();
 
             Random random = new Random();
+            int contador = 0;
 
             LoteService loteService = new LoteService();
             for (Produto produto : produtos) {
-                Lote lote1 = new Lote(produtoService.buscarPorNome(produto.getNome()).get(0), LocalDate.now(),
+                Lote lote1 = new Lote(
+                        produtoService.obterPorID(produto.getId()),
+                        LocalDate.now(),
                         random.nextInt(0, 1000),
                         LocalDate.now().plusMonths(6));
-                Lote lote2 = new Lote(produtoService.buscarPorNome(produto.getNome()).get(0),
-                        LocalDate.now().minusDays(10), random.nextInt(0, 100), LocalDate.now().plusMonths(4));
+                System.out.println("Adicionado ao lote1 " + lote1.getProduto().getNome());
+                Lote lote2 = new Lote(
+                        produtoService.obterPorID(produto.getId()),
+                        LocalDate.now().minusDays(10),
+                        random.nextInt(0, 100),
+                        LocalDate.now().plusMonths(
+                                random.nextInt(0, 12)));
+                System.out.println("Adicionado ao lote2 " + lote2.getProduto().getNome());
+                System.out.println("Contador: " + contador);
+                contador += 1;
+                System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
                 loteService.incluirAtomico(lote1);
                 loteService.incluirAtomico(lote2);
             }
