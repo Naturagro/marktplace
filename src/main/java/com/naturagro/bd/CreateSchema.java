949c1bcf84d82011d01816978ce0c25342607c4b
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-02-20 12:13:16 -0300

    simplificada a classe

--- src/main/java/com/naturagro/bd/CreateSchema.java
+++ src/main/java/com/naturagro/bd/CreateSchema.java
@@ -4,30 +4,18 @@ import java.sql.*;
 
 public class CreateSchema {
 
-    // URL de conexão JDBC para o MySQL
     String url = "jdbc:mysql://localhost:3306/";
-    // Nome de usuário do MySQL
     String user = "root";
-    // Senha do MySQL
     String password = "123456";
-
     Connection connection = null;
     Statement statement = null;
 
     public void createSchema() {
-
         try {
-            // Estabelecer a conexão
             connection = DriverManager.getConnection(url, user, password);
-            // Criar um Statement
             statement = connection.createStatement();
-
-
-            // Executar uma query SQL (por exemplo, selecionar todos os funcionários)
             String sql = "CREATE DATABASE IF NOT EXISTS naturagro";
-
             statement.executeUpdate(sql);
-
             } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
