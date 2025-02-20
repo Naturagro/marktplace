8eb30da3f70965417eb3488c2205d47b851a366a
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-02-20 12:17:04 -0300

    Main sendo estruturado para chamada da aplicação

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -1,7 +1,12 @@
 package com.main;
 
+import com.naturagro.bd.PopularBD;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("O projeto começará aqui");
+
+        PopularBD popularBD = new PopularBD();
+        popularBD.popular();
     }
 }
